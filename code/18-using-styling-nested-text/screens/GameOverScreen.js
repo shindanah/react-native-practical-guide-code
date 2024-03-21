6d507258f944b4ec7b8bf6ff9933c1d27c597b56
danah shin <97859846+shindanah@users.noreply.github.com>
2024-03-21 23:32:26 +0900

    Update GameOverScreen.js

--- code/18-using-styling-nested-text/screens/GameOverScreen.js
+++ code/18-using-styling-nested-text/screens/GameOverScreen.js
@@ -1,24 +1,19 @@
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { Text, View, Image } from "react-native";
+import { StyleSheet } from "react-native";
 
-import Title from '../components/ui/Title';
-import PrimaryButton from '../components/ui/PrimaryButton';
-import Colors from '../constants/colors';
+import Colors from "../util/colors";
+import Title from "../components/ui/Title";
 
 function GameOverScreen() {
   return (
     <View style={styles.rootContainer}>
-      <Title>GAME OVER!</Title>
-      <View style={styles.imageContainer}>
+      <Title>Game Over!</Title>
+      <View styles={styles.imageContainer}>
         <Image
           style={styles.image}
-          source={require('../assets/images/success.png')}
+          source={require("../assets/images/success.png")}
         />
       </View>
-      <Text style={styles.summaryText}>
-        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
-        guess the number <Text style={styles.highlight}>Y</Text>.
-      </Text>
-      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -29,8 +24,8 @@ const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
     padding: 24,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   imageContainer: {
     width: 300,
@@ -38,21 +33,12 @@ const styles = StyleSheet.create({
     borderRadius: 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
-    overflow: 'hidden',
+    overflow: "hidden",
     margin: 36,
   },
   image: {
-    width: '100%',
-    height: '100%',
-  },
-  summaryText: {
-    fontFamily: 'open-sans',
-    fontSize: 24,
-    textAlign: 'center',
-    marginBottom: 24
-  },
-  highlight: {
-    fontFamily: 'open-sans-bold',
-    color: Colors.primary500,
+    width: "100%",
+    height: "100%",
   },
 });
+
